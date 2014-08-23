291e3306501c8061bffca9cc62d2080a01279d4c
mrpoodlepants <jbgaynor@ieee.org>
2014-08-23 07:49:15 -0400

    Initial commit for database management.

---
+++ common.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python2
+"""Initialize and define cinch-stats database."""
+
+# Construct sqlite database
+from dal import DAL, Field
+
+db = DAL('sqlite://storage.sqlite',folder='db')
+
+# Database table definitions -- defined here rather than in the relevant
+# modules to allow for reference fields without worrying about the import sequence.
+db.define_table(
+    'Cards',
+    Field('Name', 'string', required=True),
+    Field('Quantity', 'integer', required=True),
+)
+
+db.define_table(
+    'Transactions',
+    Field('card_id', 'reference Cards', required=True),
+    Field('mu', 'float', required=True),
+    Field('sigma', 'float', required=True)
+)
--- setup.py
+++ setup.py
@@ -1,24 +1,54 @@
 #!/usr/bin/python2
 # Executive summary for help page.
-DESC = '''cube-stats: Verify database structure and update cube lists.'''
+DESC = '''cube-stats: Manage the cube list.'''
 
-
-
-
+#------------------------------------------------------------------------------#
+# Parse command-line arguments and (if called directly) set up logging.
 import argparse
 import logging
-log = logging.getLogger(__name__)
-LOG_SHORT ={'d':'DEBUG', 'i':'INFO', 'w':'WARNING', 'e':'ERROR', 'c':'CRITICAL'}
+import logging.config
+# Change root logger level in logging.config to view debug/info from modules.
+logging.config.fileConfig('logging.config')
 
-def main():
-    pass
+log = logging.getLogger("setup")
+LOG_SHORT ={'d':logging.DEBUG,
+            'i':logging.INFO,
+            'w':logging.WARNING,
+            'e':logging.ERROR,
+            'c':logging.CRITICAL}
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description = DESC)
     parser.add_argument("-l", "--loglevel",
                         help="set log level (default=WARNING)", type=str,
                         choices = list(LOG_SHORT.keys()), default='w')
+    parser.add_argument("cards", help="filename of add/change file", type=str)
     args = parser.parse_args()
-    logging.basicConfig(level = LOG_SHORT[args.loglevel])
+    log.level = LOG_SHORT[args.loglevel]
+#------------------------------------------------------------------------------#
 
-    main()
+# DAL setup/database init
+import common
+
+
+def update(filename):
+    '''Change the cube list to reflect the contents of FILENAME.'''
+    log.debug(filename)
+    f = open(filename, 'r')
+    cardData = f.readlines()
+    for i in range(len(cardData)):
+        cardData[i] = cardData[i].strip()
+    log.debug(cardData)
+
+# These functions currently act as placeholders and will be implemented should
+# the need arise.
+def add(filename):
+    '''Add cards found in FILENAME to the cube (increase quantity).'''
+    raise NotImplementedError
+
+def drop(filename):
+    '''Remove cards found in FILENAME from the cube. (set qty to 0?)'''
+    raise NotImplementedError
+
+if __name__ == "__main__":
+    add(args.cards)
