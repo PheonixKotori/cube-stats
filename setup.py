48a8151af33b72de0b7dd53cd71ce2681d211d8a
mrpoodlepants <jbgaynor@ieee.org>
2014-08-24 21:15:52 -0400

    Implemented update in setup.py - can now import a cube list.

--- setup.py
+++ setup.py
@@ -28,17 +28,67 @@ if __name__ == "__main__":
 #------------------------------------------------------------------------------#
 
 # DAL setup/database init
-import common
+from common import db
 
 
 def update(filename):
-    '''Change the cube list to reflect the contents of FILENAME.'''
-    log.debug(filename)
+    '''Change the cube list to reflect the contents of FILENAME.
+    FILENAME: a list of cardnames, one per line. No quantity data - multiples
+    must be listed separately for each instance. This matches CubeTutor's output
+    as of August 2014.
+    
+    This function:
+    1. Iterates through the list and builds a {name: quantity} dictionary.
+    2. Iterates through the cards table, setting quantities to 0 on all cards
+    not appearing in the dictionary.
+    3. Iterates through the dictionary, creating or updating rows as needed.
+
+    This function does not add, update, or change card attributes (color etc.).
+    '''
+
+    log.debug("Reading from %s", filename)
     f = open(filename, 'r')
     cardData = f.readlines()
+    log.info("cardData indicates a %s-card cube.", len(cardData))
+
+    # Step 1
+
+    newCards = {} # empty dictionary to hold name:qty keypairs
     for i in range(len(cardData)):
         cardData[i] = cardData[i].strip()
-    log.debug(cardData)
+        if cardData[i] in newCards:    # Duplicate cardname found...
+            newCards[cardData[i]] += 1 # so increment the quantity.
+        else:
+            newCards[cardData[i]] = 1  # Otherwise add a key with qty=1.
+    log.warning("newCards indicates a %s-card cube.", sum(newCards.values()))
+
+    # Step 2
+
+    cut_cards = added_cards = 0 # Count number of changed cards for logging.
+    for row in db().select(db.Cards.Name, db.Cards.Quantity):
+        if row.Name not in newCards and row.Quantity != 0: # Card has been cut
+            db(db.Cards.Name == row.Name).update(Quantity = 0)
+            cut_cards += row.Quantity
+        else:
+            added_cards -= row.Quantity
+            # This subtracts out the existing records' quantities. The total
+            # number of added cards is the sum of this (negative or 0) and the
+            # update_or_insert quantity sum in step 3.
+
+    log.warning("Cutting %s cards...", cut_cards)
+
+    # Step 3
+
+    for cardname in newCards:
+        db.Cards.update_or_insert(db.Cards.Name == cardname, Name = cardname,
+                                  Quantity = newCards[cardname])
+        added_cards += newCards[cardname]
+
+    log.warning("Adding %s cards...", added_cards)
+
+    db.commit()
+
+    return
 
 # These functions currently act as placeholders and will be implemented should
 # the need arise.
@@ -51,4 +101,4 @@ def drop(filename):
     raise NotImplementedError
 
 if __name__ == "__main__":
-    add(args.cards)
+    update(args.cards)
