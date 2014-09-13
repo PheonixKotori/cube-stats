48fb4427627d7d9730f6f91d94658979cf45b84b
Michael Garee <m.garee@gmail.com>
2014-09-13 11:34:49 -0400

    adds unit tests for importing a card list.

--- tests.py
+++ tests.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python2
 """Unit tests for cube-stats."""
 
+import sys
 import unittest
 
-
 import setup
+
 
 db = None  # Set by configDB
 
 # The cards file currently has 6 lines, 1 duplicate (Swamp), and ends in a newline character.
-cards_file = "test_cardlist.txt"
+cards_file = "test_support/test_cardlist.txt"
+cards_file2 = "test_support/test_cardlist2.txt"
 
 
 def getTempDB(showerr=False):
@@ -56,10 +58,48 @@ class MainTest(unittest.TestCase):
 
     def setUp(self):
         """do stuff to prepare for each test"""
-        for table in rr.db.tables:
-            rr.db[table].truncate("CASCADE")
-            
+        for table in setup.db.tables:
+            setup.db[table].truncate("CASCADE")
+        
+    def tearDown(self):
+        """do stuff to clean up after each test"""
+        pass
+        
+    def testLoadCardList(self):
+        """test that loading a test list of cards to an empty database works"""
+        # cards_file has 5 cards (6 lines and a duplicate swamp)
+        setup.update(cards_file)
+        
+        cards = setup.db(setup.db.Cards.id > 0).select()
+        self.assertEqual(len(cards), 5)
+        
+        swamp = setup.db(setup.db.Cards.Name == "Swamp").select().first()
+        self.assertEqual(swamp.Quantity, 2)
+    
+        # Ensure the rest are active w/ quantity 1
+        for card in cards:
+            if card.Name != "Swamp":
+                self.assertEqual(card.Quantity, 1)
 
+    def testUpdateCardList(self):
+        """test that database is updated properly"""
+        setup.update(cards_file)  # Tested via testLoadCardsList
+        setup.update(cards_file2)
+        
+        # cards_list2 should make the following changes against card_list:
+        # - 1x Swamp
+        # - 1x Faith's Fetters
+        # + 1x Island
+        cards = setup.db(setup.db.Cards.id > 0).select()
+
+        ff = setup.db(setup.db.Cards.Name == "Faith's Fetters")
+        self.assertEqual(ff.count(), 1)
+
+        self.assertEqual(1, setup.db(setup.db.Cards.Name == "Swamp").select().first().Quantity)
+        self.assertEqual(0, setup.db(setup.db.Cards.Name == "Faith's Fetters").select().first().Quantity)
+        self.assertEqual(1, setup.db(setup.db.Cards.Name == "Island").select().first().Quantity)
+        
+        
 if __name__ == "__main__":
-    db = getTempDB(showerr=True)
-    print db
+    setup.db = getTempDB(showerr=True)
+    unittest.main()
