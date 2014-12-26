1e28c201ce0140dd5d34a65e710c87f0e050a495
PheonixKotori <m.garee@gmail.com>
2014-12-26 12:08:49 -0800

    Added simple data and chart to index.

--- web2pyApp/controllers/default.py
+++ web2pyApp/controllers/default.py
@@ -1,12 +1,30 @@
 # -*- coding: utf-8 -*-
 
-def index():
-    """
-    example action using the internationalization operator T and flash
-    rendered by views/default/index.html or views/generic.html
+# copied from draft.py
+DEFAULT_MU = 25.0
+DEFAULT_SIGMA = 25.0/3
 
-    if you need a simple wiki simply replace the two lines below with:
-    return auth.wiki()
-    """
-    response.flash = T("Welcome to web2py!")
-    return dict(message=db(db.Cards.id == 1).select().first())
+
+def index():
+    """Main page."""
+    colors = 'W U B R G N'.split(' ')
+    top_cards = []
+    top_cards_by_color = []
+
+    num_top_cards = 20  # How many cards make up the top
+
+    # How to break ties?
+
+    # mostly copied from draft.py
+    top_cards = db(db.Cards.Quantity > 0).select(db.Cards.Name,
+        db.Transactions.mu.coalesce(DEFAULT_MU).with_alias('mu'),
+        db.Transactions.sigma.coalesce(DEFAULT_SIGMA).with_alias('sigma'),
+        left=db.Transactions.on(db.Cards.id == db.Transactions.card_id),
+        orderby=db.Transactions.mu|db.Transactions.sigma|db.Transactions.timestamp,
+        limitby=(0, num_top_cards)
+        )
+
+    return dict(colors=colors,
+                top_cards=top_cards,
+                top_cards_by_color=top_cards_by_color
+                )
