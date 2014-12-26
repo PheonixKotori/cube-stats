6210ec2826ee65d3903cc834d11ae213f6365f3e
PheonixKotori <m.garee@gmail.com>
2014-12-26 12:42:59 -0800

    Fixed index to show actual data.

--- web2pyApp/controllers/default.py
+++ web2pyApp/controllers/default.py
@@ -11,20 +11,49 @@ def index():
     top_cards = []
     top_cards_by_color = []
 
-    num_top_cards = 20  # How many cards make up the top
+    num_top_cards = 12  # How many cards make up the top
 
-    # How to break ties?
+    snapshot = take_snapshot(None)
 
-    # mostly copied from draft.py
-    top_cards = db(db.Cards.Quantity > 0).select(db.Cards.Name,
-        db.Transactions.mu.coalesce(DEFAULT_MU).with_alias('mu'),
-        db.Transactions.sigma.coalesce(DEFAULT_SIGMA).with_alias('sigma'),
-        left=db.Transactions.on(db.Cards.id == db.Transactions.card_id),
-        orderby=db.Transactions.mu|db.Transactions.sigma|db.Transactions.timestamp,
-        limitby=(0, num_top_cards)
-        )
-
+    top_cards = snapshot[0:num_top_cards]
+    
     return dict(colors=colors,
                 top_cards=top_cards,
                 top_cards_by_color=top_cards_by_color
                 )
+
+
+def take_snapshot(timestamp=None):
+    """Return snapshot of cube stats for a given timestamp, or now if None.
+
+    Args:
+      timestamp (str): Of the form "2014-12-26 20:09:52.100875".
+
+    Returns:
+      list of Storage objects: Objects with properties name, mu, and sigma.
+
+    """
+    from gluon.storage import Storage
+
+    if timestamp is None:
+        timestamp = db(db.Transactions.id > 0).select(
+            db.Transactions.timestamp,
+            orderby=~db.Transactions.timestamp,
+            limitby=(0,1)).first().timestamp
+
+    active_cards_with_ratings = db(db.Cards.Quantity > 0).select(db.Cards.Name,
+        db.Transactions.mu.coalesce(DEFAULT_MU).with_alias('mu'),
+        db.Transactions.sigma.coalesce(DEFAULT_SIGMA).with_alias('sigma'),
+        left=db.Transactions.on(db.Cards.id == db.Transactions.card_id),
+        orderby=db.Transactions.timestamp)
+
+    # Only retains most recent timestamp due to select being ordered by same
+    acwrdict = {row['Cards.Name']:(row['mu'], row['sigma'])
+                for row in active_cards_with_ratings}
+
+    # Turn dict into ordered list
+    data = [Storage(name=name, mu=acwrdict[name][0],
+                    sigma=acwrdict[name][1]) for name in acwrdict.keys()]
+    data.sort(key=lambda x: x.mu, reverse=True)
+
+    return data
